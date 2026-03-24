6caf9666e46ffd979deb72fc1c30a3585425577d
Janssen Batista <batistajanssen.dev@gmail.com>
2026-03-24 14:28:10 -0300

    feat: add CT-E2E-001 test case

--- cypress/e2e/calculadora-de-adipometro.cy.ts
+++ cypress/e2e/calculadora-de-adipometro.cy.ts
@@ -1,7 +1,22 @@
 /// <reference types="cypress" />
 
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io');
+describe('Calculador de Adipômetro', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('CT-E2E-001 - Estado inicial da tela', () => {
+    cy.obterDataTestId('protocolo').contains('Pollock 7 Dobras');
+    cy.obterDataTestId('genero').find('option:selected').should('have.text', 'Masculino');
+    cy.verificarDobrasVisiveis([
+      'triceps',
+      'subescapular',
+      'suprailiaca',
+      'abdominal',
+      'coxa',
+      'peitoral',
+      'axilarMedia',
+    ]);
+    cy.obterDataTestId('resultado').contains('--');
   });
 });
