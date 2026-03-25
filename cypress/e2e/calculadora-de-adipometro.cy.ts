de596cc21f859ba4e7e9735f9f4dfa6f79b2e237
Janssen Batista <batistajanssen.dev@gmail.com>
2026-03-25 12:01:14 -0300

    feat: add CT-E2E-002 test case

--- cypress/e2e/calculadora-de-adipometro.cy.ts
+++ cypress/e2e/calculadora-de-adipometro.cy.ts
@@ -1,5 +1,31 @@
 /// <reference types="cypress" />
 
+interface Dados {
+  genero: string;
+  idade: number;
+  triceps: number;
+  subescapular: number;
+  suprailiaca: number;
+  abdominal: number;
+  coxa: number;
+  peitoral: number;
+  axilarMedia: number;
+  panturrilha: number;
+}
+
+const DadoMasculino: Dados = {
+  genero: 'masculino',
+  idade: 28,
+  triceps: 12,
+  subescapular: 10,
+  suprailiaca: 14,
+  abdominal: 18,
+  coxa: 20,
+  peitoral: 11,
+  axilarMedia: 9,
+  panturrilha: 13,
+};
+
 describe('Calculador de Adipômetro', () => {
   beforeEach(() => {
     cy.visit('/');
@@ -18,5 +44,34 @@ describe('Calculador de Adipômetro', () => {
       'axilarMedia',
     ]);
     cy.obterDataTestId('resultado').contains('--');
+  });
+
+  it('CT-E2E-002 - Pollock 7 masculino com dados válidos', () => {
+    const {
+      genero,
+      idade,
+      triceps,
+      subescapular,
+      suprailiaca,
+      abdominal,
+      coxa,
+      peitoral,
+      axilarMedia,
+    } = DadoMasculino;
+
+    cy.obterDataTestId('protocolo').select('Pollock 7 Dobras');
+    cy.obterDataTestId('genero').select(genero);
+
+    cy.obterDataTestId('idade').type(idade);
+    cy.obterDataTestId('triceps').type(triceps);
+    cy.obterDataTestId('subescapular').type(subescapular);
+    cy.obterDataTestId('suprailiaca').type(suprailiaca);
+    cy.obterDataTestId('abdominal').type(abdominal);
+    cy.obterDataTestId('coxa').type(coxa);
+    cy.obterDataTestId('peitoral').type(peitoral);
+    cy.obterDataTestId('axilarMedia').type(axilarMedia);
+
+    cy.obterDataTestId('titulo-resultado').should('have.text', 'Pollock 7 Dobras');
+    cy.obterDataTestId('resultado').should('have.text', '13.53 %');
   });
 });
